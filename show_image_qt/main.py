6175c3fa3d71d38e368a6c7274d3494046070687
Mohammad Bahmani <bahmani.m.n@gmail.com>
2023-09-11 14:31:40 +0200

    a function to call the photos

--- show_image_qt/main.py
+++ show_image_qt/main.py
@@ -14,9 +14,18 @@ class ShowImage(QWidget, Ui_Imagesite):
         
         # self.label = QLabel(self)
         # pixmap = QPixmap('happy_images/happy_guy.jpg')
-        pixmap = QPixmap('sad_images/sad_man.jpg')
+        emotion_image = self.sad_or_happy("sad")
+        pixmap = QPixmap(emotion_image)
         # TODO: Write if Statement to cahnge between set of happy and sad images
         self.label.setPixmap(pixmap)
+
+    def sad_or_happy(self, emotion):
+        if emotion == "happy":
+            return "happy_images/happy_guy.jpg"
+        elif emotion == "sad":
+            return "sad_images/sad_man.jpg"
+        else: 
+            return "Please specify your emotions!!!"
 
 app = QtWidgets.QApplication(sys.argv)
 
