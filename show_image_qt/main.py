516f18eaa5387da1baa7da5a6f91d5fba1f2bed5
Mohammad Bahmani <bahmani.m.n@gmail.com>
2023-11-07 10:12:43 +0100

    last commit

--- show_image_qt/main.py
+++ show_image_qt/main.py
@@ -12,21 +12,16 @@ class ShowImage(QWidget, Ui_Imagesite):
         self.setupUi(self)
         self.setWindowTitle("User data")
         
-        # self.label = QLabel(self)
-        # pixmap = QPixmap('happy_images/happy_guy.jpg')
-        # emotion_image = self.sad_or_happy("sad")
-        # pixmap = QPixmap(emotion_image)
-        # TODO: Write if Statement to cahnge between set of happy and sad images
         self.pushButton_happy.clicked.connect(self.get_happy)
-        print(self.get_happy)
+        # print(self.get_happy)
         self.pushButton_sad.clicked.connect(self.get_sad)
-        print(self.get_sad)
+        # print(self.get_sad)
         
-        print(self.pushButton_happy.text())
-        print(self.pushButton_sad.text())
-        # self.label.setPixmap(self.pixmap)
+        # print(self.pushButton_happy.text())
+        # print(self.pushButton_sad.text())
         
-
+    # TODO set API for each of these emotions to get the image
+    # TODO WORK with APIs and then rewrite the app in microservices
     def get_happy(self):
         pixmap = QPixmap('happy_images/happy_guy.jpg')
         self.label.setPixmap(pixmap)
@@ -35,6 +30,8 @@ class ShowImage(QWidget, Ui_Imagesite):
         pixmap = QPixmap("sad_images/sad_man.jpg")
         self.label.setPixmap(pixmap)
         
+
+    #  TODO SETUP the English Teacher with QT Designer and API call to OpenAI
 
     # def sad_or_happy(self, emotion="sad"):
     #     if emotion == "happy":
